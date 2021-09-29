0bd272cace299f1442ab803859e894500216d628
akshay kavalagi <veereshkavalagi31@gmail.com>
2021-09-29 23:00:04 +0530

    blogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -64,7 +64,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: 'http://localhost:1337',
+        apiURL: process.env.STRAPI_API_URL,
         queryLimit: 1000, // Default to 100
         collectionTypes: [
           `author`,
